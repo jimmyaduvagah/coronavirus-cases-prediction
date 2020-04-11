230cd43088e909c4e1c27d734191efad837a6d8a
jimmy <jimmy.aduvagah@savannahinformatics.com>
2020-04-11 08:41:18 +0300

    fire fighting

--- src/estimator.py
+++ src/estimator.py
@@ -45,7 +45,7 @@ def estimator(data):
     def get_dollars_in_flight(estimate_type="impact"):
         avg_income = data["region"]["avgDailyIncomeInUSD"]
         cases = convid_cases if estimate_type == "impact" else severe_cases
-        return round((cases * 0.65 * avg_income * get_days()), 2)
+        return round((cases * 0.65 * avg_income * get_days()))
 
     data_to_return = {
         "data": data,
